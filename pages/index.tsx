80e97f033f2fbb0fe50b3ed135b1ede5df2c3c6d
salvatorediocaro <salvatore.diocaro@gmail.com>
2024-04-19 13:26:15 +0200

    B2BP-521 - created dashboard example in iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,58 @@
-import React from "react";
+// import React from "react";
+// import Head from "next/head";
+// import Navbar from "@/app/components/Navbar";
+// import styles from "./index.module.css";
+// import Footer from "@/app/components/Footer";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <title>Home</title>
+//       </Head>
+//       <Navbar />
+//       <div className={styles.flexContainer}>
+//         <iframe id="idIframe" src="/iframe" className={styles.flexIframe} />
+//       </div>
+//       <Footer></Footer>
+//     </>
+//   );
+// }
+
+
+import React, { useLayoutEffect } from "react";
 import Head from "next/head";
 import Navbar from "@/app/components/Navbar";
 import styles from "./index.module.css";
 import Footer from "@/app/components/Footer";
 
 export default function Home() {
+
+  const resizeIFrameToFitContent = ( iFrame: HTMLElement ) => {
+    // @ts-ignore
+    iFrame.height = iFrame.contentDocument.body.scrollHeight;
+  }
+
+  useLayoutEffect(() => {
+    const iFrame = document.getElementById('idIframe');
+    if (iFrame === null) return;
+    resizeIFrameToFitContent( iFrame );
+
+      // or, to resize all iframes:
+      // const iFrames = document.querySelectorAll("iframe");
+      // for( let i = 0; i < iFrames.length; i++) {
+      //     resizeIFrameToFitContent( iFrames[i] );
+      // }
+  }, [])
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
       <Navbar />
-      <div className={styles.flexContainer}>
-        <iframe id="idIframe" src="/iframe" className={styles.flexIframe} />
-      </div>
-      <Footer></Footer>
+      <iframe id="idIframe" src="/iframe" className={styles.flexIframe} />
+      <Footer />
     </>
   );
 }
